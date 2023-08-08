97c21bf3780f0c1ac7a3504a525f29f41d589242
mmj030703 <mayankjain7738@gmail.com>
2023-08-08 21:21:10 +0530

    Added PHP Folder and added insert_categories.php & insert_brands.php files

---
+++ ADMIN/PHP/insert_brands.php
@@ -0,0 +1,15 @@
+<form action="POST" class="mb-2">
+    <!-- ******************************************** || Input Field Starts Here || *********************************************** -->
+    <div class="input-group mb-2">
+        <span class="input-group-text bg-primary text-light border border-primary" id="addon-wrapping"><i class="fa-solid fa-receipt"></i></span>
+        <input type="text" class="form-control border border-primary" name="brand" placeholder="Insert Brand..." aria-label="Brands" aria-describedby="basic-addon1">
+    </div>
+    <!-- ******************************************** || Input Field Ends Here || *********************************************** -->
+
+    <!-- ******************************************** || Insert Button Starts Here || *********************************************** -->
+    <div class="input-group mb-2">
+        <!-- <input type="submit" class="form-control bg-info" name="insert_brand_button" value="Insert Brand" aria-describedby="basic-addon1"> -->
+        <button class="bg-primary p-2 border-0 text-light my-2">Insert Brand</button>
+    </div>
+    <!-- ******************************************** || Insert Button Ends Here || *********************************************** -->
+</form>
---
+++ ADMIN/PHP/insert_categories.php
@@ -0,0 +1,16 @@
+<form action="POST" class="mb-2">
+    <!-- ******************************************** || Input Field Starts Here || *********************************************** -->
+    <div class="input-group mb-2">
+        <span class="input-group-text bg-primary text-light border border-primary" id="addon-wrapping"><i class="fa-solid fa-receipt"></i></span>
+        <input type="text" class="form-control border border-primary" name="category_title" placeholder="Insert Category..." aria-label="Categories" aria-describedby="basic-addon1">
+    </div>
+    <!-- ******************************************** || Input Field Ends Here || *********************************************** -->
+
+    <!-- ******************************************** || Insert Button Starts Here || *********************************************** -->
+    <div class="input-group mb-2">
+        <!-- <input type="submit" class="form-control bg-info" name="insert_category_button" value="Insert Category" aria-describedby="basic-addon1"> -->
+        <button class="bg-primary p-2 border-0 text-light my-2">Insert Category</button>
+    </div>
+    <!-- ******************************************** || Insert Button Ends Here || *********************************************** -->
+
+</form>
