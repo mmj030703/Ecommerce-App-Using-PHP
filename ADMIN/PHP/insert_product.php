e96dc0fa948a68b039ab522ba3c50c6565be377b
mmj030703 <mayankjain7738@gmail.com>
2023-08-11 22:58:31 +0530

    Updated insert_product.php & added categories and brands in select tag respectively from Database and completed the logic of PHP to insert product details in Products table of Database.

--- ADMIN/PHP/insert_product.php
+++ ADMIN/PHP/insert_product.php
@@ -1,3 +1,61 @@
+<!-- PHP Code -->
+<?php
+    include('../../includes/connect.php');
+
+    // If the Submit button is clicked then execute the following block. 
+    if(isset($_POST['insert_product'])) {
+        // Accessing Products Details
+        $product_title = $_POST['product_title'];
+        $product_description = $_POST['product_description'];
+        $product_keywords = $_POST['product_keywords'];
+        $product_category = $_POST['product_category'];
+        $product_brand = $_POST['product_brand'];
+        $product_price = $_POST['product_price'];
+        $product_status = 'true';
+
+        // Accessing Images using name & tmp_name attribute.
+        $product_image1 = $_FILES['product_image_1']['name'];
+        $product_image2 = $_FILES['product_image_2']['name'];
+        $product_image3 = $_FILES['product_image_3']['name'];
+
+        $tmp_image1 = $_FILES['product_image_1']['tmp_name'];
+        $tmp_image2 = $_FILES['product_image_2']['tmp_name'];
+        $tmp_image3 = $_FILES['product_image_3']['tmp_name'];
+
+        // Contidion for Empty Fields
+        if($product_title == "" or $product_description == "" or $product_keywords == "" or $product_category == "" or $product_brand == "" or $product_price == "" or $product_image1 == "" or $product_image2 == "" or $product_image3 == "") {
+            echo "
+                <script>
+                    alert('Please Fill all the available Fields.');
+                </script>
+            ";
+            exit();
+        }
+        else {
+            // Moves the file specified as 1st parameter in the location specified in the 2nd parameter.
+            move_uploaded_file($tmp_image1, "../Images/$product_image1");
+            move_uploaded_file($tmp_image2, "../Images/$product_image2");
+            move_uploaded_file($tmp_image3, "../Images/$product_image3");
+
+            // Query to insert product details into products table
+            $query_to_insert_product_data = "insert into `products` (product_title, product_description, product_keywords, category_id, brand_id, product_image1, product_image2, product_image3, product_price, date, status) values ('$product_title', '$product_description', '$product_keywords', '$product_category', '$product_brand', '$product_image1', '$product_image2', '$product_image3', '$product_price', 'NOW()', '$product_status')";
+
+            // Result of above query
+            $query_result = mysqli_query($con, $query_to_insert_product_data);
+
+            // If query is executed then alert the success message.
+            if($query_result) {
+                echo "
+                    <script>
+                        alert('$product_title Inserted Successfully.');
+                    </script>
+                ";
+            }
+        }
+    }
+?>
+<!-- PHP Code -->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -42,12 +100,31 @@
                 <div class="categories_container form-outline mb-4 w-50 m-auto">
                     <select name="product_category" class="form-select">
                         <option value="" class="m-4">Select a Category</option>
-                        <option value="" class="m-4">Category 1</option>
-                        <option value="" class="m-4">Category 2</option>
-                        <option value="" class="m-4">Category 3</option>
-                        <option value="" class="m-4">Category 4</option>
-                        <option value="" class="m-4">Category 5</option>
-                        <option value="" class="m-4">Category 6</option>
+
+                        <!-- PHP Code -->
+                        <?php
+                            // Query to Select all the Categories from Database.
+                            $query_to_select_all_categories = "select * from `categories`";
+
+                            // Result of above Query.
+                            $query_result = mysqli_query($con, $query_to_select_all_categories);
+
+                            // Running while loop till the end of categories table.
+                            while($table_row = mysqli_fetch_assoc($query_result)) {
+                                // Accessing category title
+                                $category_title = $table_row['category_title'];
+
+                                // Accessing category id
+                                $category_id = $table_row['category_id'];
+
+                                // Adding category title to select tag in DOM.
+                                echo "
+                                    <option value='$category_id' class='m-4'>$category_title</option>
+                                ";
+                            }
+                        ?>
+                        <!-- PHP Code -->
+
                     </select>
                 </div> 
 
@@ -55,12 +132,19 @@
                 <div class="brands_container form-outline mb-4 w-50 m-auto">
                     <select name="product_brand" class="form-select">
                         <option value="" class="m-4">Select a Brand</option>
-                        <option value="" class="m-4">Brand 1</option>
-                        <option value="" class="m-4">Brand 2</option>
-                        <option value="" class="m-4">Brand 3</option>
-                        <option value="" class="m-4">Brand 4</option>
-                        <option value="" class="m-4">Brand 5</option>
-                        <option value="" class="m-4">Brand 6</option>
+                        <?php
+                            $query_to_select_all_brands = "select * from `brands`";
+                            $query_result = mysqli_query($con, $query_to_select_all_brands);
+
+                            while($table_row = mysqli_fetch_assoc($query_result)) {
+                                $brand_title = $table_row['brand_title'];
+                                $brand_id = $table_row['brand_id'];
+
+                                echo "
+                                    <option value='$brand_id' class='m-4'>$brand_title</option>
+                                ";
+                            }
+                        ?>
                     </select>
                 </div> 
 
