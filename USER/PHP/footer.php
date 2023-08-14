439ef1999dc326e660bbab1de021284a2623bf37
mmj030703 <mayankjain7738@gmail.com>
2023-08-14 19:09:32 +0530

    Added PHP Folder in USER Folder & added footer.php in PHP Folder

---
+++ USER/PHP/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <div class="footer container-fluid bg-primary text-center">
+        <h4 class="text-black pt-2">All Rights Reserved &copy; Shoppers Bay.</h4>
+        <p class="fs-5 m-2">Made by Mayank M Jain ♥</p>
+    </div>
+</footer>
