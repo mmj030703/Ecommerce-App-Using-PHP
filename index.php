dfff5a266e49b799d0f68687ae6f5773a447ca00
mmj030703 <mayankjain7738@gmail.com>
2023-08-14 19:08:35 +0530

    Updated index.php
    Updated the name of function getAllProducts in function call and included the footer.php to add the footer section.

--- index.php
+++ index.php
@@ -30,18 +30,18 @@
         <!-- ******************************************** || Navbar Starts Here || *********************************************** -->
         <nav class="navbar navbar-expand-lg bg-primary">
             <div class="container-fluid">
-                <img class="logo" src="./USER/Images/Logo.png" alt="Logo">
-                <a class="navbar-brand" href="#">Shoppers Bay</a>
+                <a href="index.php"><img class="logo" src="./USER/Images/Logo.png" alt="Logo"></a>
+                <a class="navbar-brand" href="index.php">Shoppers Bay</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Products</a>
+                            <a class="nav-link" href="display_all_products.php">Products</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Register</a>
@@ -104,7 +104,7 @@
                         <!-- PHP Code -->
                         <?php
                             // Function to get all the products from database and insert in the DOM. 
-                            getAllProducts();
+                            getProducts();
                             getUniqueProducts();
                         ?>
                         <!-- PHP Code -->
@@ -153,10 +153,12 @@
     <!-- ******************************************** || Main Ends Here || *********************************************** -->
 
     <!-- ******************************************** || Footer Starts Here || *********************************************** -->
-    <div class="footer container-fluid bg-primary text-center">
-        <h4 class="text-black pt-2">All Rights Reserved &copy; Shoppers Bay.</h4>
-        <p class="fs-5 m-2">Made by Mayank M Jain ♥</p>
-    </div>
+    <!-- PHP Code -->
+    <?php
+        // File containing code for footer section.
+        include('./USER/PHP/footer.php');
+    ?>
+    <!-- PHP Code -->
     <!-- ******************************************** || Footer Ends Here || *********************************************** -->
 
     <!-- BootStrap Script Link -->
