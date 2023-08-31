ca7657027ca9cd1849667a25e49096d55037bb5f
mmj030703 <mayankjain7738@gmail.com>
2023-08-31 21:29:54 +0530

    Added php code for adding user details in the database.

--- users_area/user_registration.php
+++ users_area/user_registration.php
@@ -1,6 +1,7 @@
 <!-- PHP Code -->
 <?php
     include('../includes/connect.php');
+    include('../Functions/common_function.php');
 ?>
 <!-- PHP Code -->
 
@@ -83,3 +84,32 @@
     <!-- ******************************************** || Main Ends Here || *********************************************** -->
 </body>
 </html>
+
+<!-- PHP Code -->
+<?php 
+    if(isset($_POST['register_user'])) {
+        $user_username = $_POST['user_username'];
+        $user_email = $_POST['user_email'];
+        $user_password = $_POST['user_password'];
+        $user_password_confirmed = $_POST['user_password_confirmed'];
+        $user_address = $_POST['user_address'];
+        $user_contact = $_POST['user_contact'];
+        $user_image = $_FILES['user_image']['name'];
+        $user_image_tmp = $_FILES['user_image']['tmp_name'];
+        $user_ip_address = getIPAddress();
+
+        move_uploaded_file($user_image_tmp, "./user_images/$user_image");
+
+        $insert_query = "insert into `user_table` (username, user_email, user_password, user_image, user_ip, user_address, user_mobile) values ('$user_username', '$user_email', '$user_password', '$user_image', '$user_ip_address', '$user_address', '$user_contact')";
+        $insert_query_result = mysqli_query($con, $insert_query);
+
+        if($insert_query_result) {
+            echo "
+                <script>
+                    alert('User Registered Successfully.');
+                </script>
+            ";
+        }
+    }
+?>
+<!-- PHP Code -->
