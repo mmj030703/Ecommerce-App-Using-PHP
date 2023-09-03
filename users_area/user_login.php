a452c8f524fc200faec611594760c9725abeb8e8
mmj030703 <mayankjain7738@gmail.com>
2023-09-03 18:46:02 +0530

    Added php code for matching the user entered details i.e. email and password from the database information.

--- users_area/user_login.php
+++ users_area/user_login.php
@@ -1,3 +1,10 @@
+<!-- PHP Code -->
+<?php 
+    include('../includes/connect.php');
+    include('../Functions/common_function.php');
+?>
+<!-- PHP Code -->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -20,7 +27,7 @@
 
             <div class="w-100 mt-4">
                 <!-- ******************************************** || Form Starts Here || *********************************************** -->
-                <form action="" method="POST" enctype="multipart/form-data">
+                <form action="" method="POST">
                     <!-- Email -->
                     <div class="email_container form-outline mb-4 w-50 m-auto">
                         <label for="email" class="form-label">Email</label>
@@ -33,9 +40,9 @@
                         <input type="password" name="user_password" id="password" class="form-control" placeholder="Enter your Password" autocomplete="off" required="required">
                     </div>
 
-                    <!-- Register Button -->
-                    <div class="register_button_container form-outline mb-4 w-50 m-auto">
-                        <input type="submit" name="register_user" class="btn btn-primary mb-3" value="Login">
+                    <!-- Login Button -->
+                    <div class="login_button_container form-outline mb-4 w-50 m-auto">
+                        <input type="submit" name="login_user" class="btn btn-primary mb-3" value="Login">
                         <p class="mx-1 fw-bold small">Don't have an account ?<a class="text-danger text-decoration-none" href="./user_registration.php"> Register</a></p>
                     </div> 
                 </form>
@@ -46,3 +53,54 @@
     <!-- ******************************************** || Main Ends Here || *********************************************** -->
 </body>
 </html>
+
+<!-- PHP Code -->
+<?php 
+    // Checking if user clicked on Login Button
+    if(isset($_POST['login_user'])) {
+        $user_email = $_POST['user_email'];
+        $user_password = $_POST['user_password'];
+        $user_ip_address = getIPAddress();
+
+        $select_query = "select * from `user_table` where user_email = '$user_email'";
+        $select_query_result = mysqli_query($con, $select_query);
+        $number_of_users = mysqli_num_rows($select_query_result);
+        $hash_password = mysqli_fetch_assoc($select_query_result)['user_password'];
+
+        $products_select_query = "select * from `cart_details` where ip_address = '$user_ip_address'";
+        $products_select_query_result = mysqli_query($con, $products_select_query);
+        $number_of_cart_products = mysqli_num_rows($products_select_query_result);
+
+        // Checking if there is a user with the entered email in the database already and the password matches the one present in the database then login the user.
+        // password_verify() function checks whether the entered passwords hash and password in database in hash format are equal or not.
+        if($number_of_users > 0 && password_verify($user_password, $hash_password)) {
+            $_SESSION['email'] = $user_email;
+            if($number_of_cart_products > 0) {
+                $_SESSION['email'] = $user_email;
+                echo "
+                    <script>
+                        alert('Login Successful !');
+                        window.open('../cart.php', '_self');
+                    </script>
+                ";
+            }
+            else {
+                $_SESSION['email'] = $user_email;
+                echo "
+                    <script>
+                        alert('Login Successful !');
+                        window.open('../index.php', '_self');
+                    </script>
+                ";
+            }
+        }
+        else {
+            echo "
+                <script>
+                    alert('Invalid Credentials !');
+                </script>
+            ";
+        }
+    }
+?>
+<!-- PHP Code -->
