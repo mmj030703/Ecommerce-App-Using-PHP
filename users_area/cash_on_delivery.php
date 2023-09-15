d271514fec84cf44618ac9797541c0a5e0ed09ff
mmj030703 <mayankjain7738@gmail.com>
2023-09-15 23:00:50 +0530

    Added code for fetching product id and product price from cart_details and products table respectively.

--- users_area/cash_on_delivery.php
+++ users_area/cash_on_delivery.php
@@ -1,11 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Welcome to Cash On Delivery.</h1>
-</body>
-</html>
+<!-- PHP Code -->
+<?php
+    include('../includes/connect.php');
+    include('../Functions/common_function.php');
+
+    if(isset($_GET['user_id'])) {
+        $user_id = $_GET['user_id'];
+    }
+
+    // Getting total items & total price of all cart items
+    $ip_address = getIPAddress();
+    $total_cart_price = 0;
+
+    $products_from_cart_query = "select * from `cart_details` where ip_address='$ip_address'"; 
+    $products_from_cart_query_result = mysqli_query($con, $products_from_cart_query);
+    $number_of_products = mysqli_num_rows($products_from_cart_query_result); 
+    
+    while($cart_detail = mysqli_fetch_assoc($products_from_cart_query_result)) {
+        $product_id = $cart_detail['product_id'];
+
+        $select_product_query = "select * from `products` where product_id=$product_id";
+        $select_product_query_result = mysqli_query($con, $select_product_query);
+
+        $product_detail = mysqli_fetch_assoc($select_product_query_result);
+        $product_price = array($product_detail['product_price']);
+        $product_values = array_sum($product_price);
+        $total_cart_price += $product_values;
+    }
+?>
+<!-- PHP Code -->
